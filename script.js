6484e51ee9cb41dbd8e69d0a893b609116f946c8
MAYENGE <mayengemichael@gmail.com>
2022-06-14 19:48:26 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+let buttons = document.querySelectorAll(".button")
+let display = document.querySelector(".display")
+
+buttons.forEach(button=>{button.addEventListener("click",el=>{
+    doCalculate(el)
+})
+})
+function doCalculate(e){
+    if(display.innerHTML=="0"){
+        display.innerHTML=""
+    }
+    switch (e.target.innerHTML){
+        case "AC":
+            display.innerHTML="0"
+            break;
+         case "DEL":
+             if(display.innerHTML){
+
+           
+            display.innerHTML=display.innerHTML.slice(0,-1)
+             }
+             if(display.innerHTML==""){
+                 display.innerHTML="0"
+             }
+             break;
+         case "=":
+             try{
+            display.innerHTML=eval(display.innerHTML)
+             }catch{
+                 display.innerHTML="Error"
+             }
+            break;
+         case "AC":
+            display.innerHTML="0"
+            break;
+        default:
+            display.innerHTML+=e.target.innerHTML            
+    }
+}
